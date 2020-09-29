b0ad64a39edc423cf78724573615453c73ef5057
figo <56029754@qq.com>
2020-09-29 13:57:49 +0800

    test11111

--- test.py
+++ test.py
@@ -1,61 +1,22 @@
-from turtle import *
+# -*- coding: utf-8 -*-
+"""1、2、3、4 组成3位数，且不能有重复的数字~！"""
 
-# 设置色彩模式是RGB:
-colormode(255)
+#import os
+#print (os.path.abspath('.'))
+#/Users/figo/PycharmProjects/Study
 
-lt(90)
+"""f = open(file='/users/figo/desktop/test.txt',mode='w')
+f.write("Alex CEO 6000\n")
+f.write("黑姑娘 行政 9000\n")
+f.write("小姐姐 CFO  9000\n")
+f.write("松岛枫 admin  10000\n")
+f.write("武藤兰 admin   11100\n")
+f.close()
+"""
 
-lv = 14
-l = 120
-s = 45
-
-width(lv)
-
-# 初始化RGB颜色:
-r = 0
-g = 0
-b = 0
-pencolor(r, g, b)
-
-penup()
-bk(l)
-pendown()
-fd(l)
-
-def draw_tree(l, level):
-    global r, g, b
-    # save the current pen width
-    w = width()
-
-    # narrow the pen width
-    width(w * 3.0 / 4.0)
-    # set color:
-    r = r + 1
-    g = g + 2
-    b = b + 3
-    pencolor(r % 200, g % 200, b % 200)
-
-    l = 3.0 / 4.0 * l
-
-    lt(s)
-    fd(l)
-
-    if level < lv:
-        draw_tree(l, level + 1)
-    bk(l)
-    rt(2 * s)
-    fd(l)
-
-    if level < lv:
-        draw_tree(l, level + 1)
-    bk(l)
-    lt(s)
-
-    # restore the previous pen width
-    width(w)
-
-speed("fastest")
-
-draw_tree(l, 4)
-
-done()
+f = open(file='/users/figo/desktop/test.txt',mode='r')
+#print(f.readline())test.py:14
+print("---------分隔符-----------")
+data = f.read()
+print(data)
+f.close()
